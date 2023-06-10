da082e62851434410c70b6c3a39157bcb2a95fed
sanjana8100 <sanjanams812@gmail.com>
2023-06-10 13:43:31 +0530

    added: UC1

--- Employee.cs
+++ Employee.cs
@@ -1,10 +1,20 @@
 ﻿namespace DND3_EmployeeWageProgram
 {
-    internal class Program
+    internal class Employee
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to Employee Wage Computation Program On Master Branch");
+            Console.WriteLine("Checking whether Employee is present or absent...");
+            int empPresent = 1;
+            Random random = new Random();
+            double empCheck = random.Next(0, 3);
+            if (empCheck == empPresent)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+                Console.WriteLine("Employee is Absent");
         }
     }
 }
