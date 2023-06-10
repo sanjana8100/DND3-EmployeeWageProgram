1b72ac722f82b882fad64f6e9157fc09fdab159e
sanjana8100 <sanjanams812@gmail.com>
2023-06-10 14:05:38 +0530

    added: UC3

--- Employee.cs
+++ Employee.cs
@@ -6,15 +6,21 @@
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program On Master Branch");
             Console.WriteLine("Checking whether Employee is present or absent...");
-            int IsPresent = 1;
+            int IsFullTime = 1;
+            int IsPartTime = 2;
             int EmpWagePerHour = 20;
             int empHrs, empWage;
             Random random = new Random();
-            double empCheck = random.Next(0, 3);
-            if (empCheck == IsPresent)
+            double empCheck = random.Next(3);
+            if (empCheck == IsFullTime)
             {
-                Console.WriteLine("Employee is Present");
+                Console.WriteLine("Employee is working Full Time.");
                 empHrs = 8;
+            }
+            else if (empCheck == IsPartTime)
+            {
+                Console.WriteLine("Employee is working Part Time.");
+                empHrs = 4;
             }
             else
             {
