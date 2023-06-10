4433fe6833eb46b6aaff0ece5c70d5119eb98cc0
sanjana8100 <sanjanams812@gmail.com>
2023-06-10 15:27:07 +0530

    added: UC5

--- Employee.cs
+++ Employee.cs
@@ -5,31 +5,37 @@
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
         public const int EMP_WAGE_PER_HOUR = 20;
+        public const int WORKING_DAYS_PER_MONTH = 20;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program On Master Branch");
 
-            int empHrs, empWage;
-            Random random = new Random();
-            double empCheck = random.Next(3);
-            switch ((int)empCheck)
+            int empHrs, empWagePerDay, empWagePerMonth = 0;
+            for (int day = 0; day <= WORKING_DAYS_PER_MONTH; day++)
             {
-                case IS_FULL_TIME:
-                    Console.WriteLine("Employee is Doing Full Time:");
-                    empHrs = 8;
-                    break;
-                case IS_PART_TIME:
-                    Console.WriteLine("Employee is Doing Part Time:");
-                    empHrs = 4;
-                    break;
-                default:
-                    Console.WriteLine("Employee is Absent:");
-                    empHrs = 0;
-                    break;
+                Random random = new Random();
+                double empCheck = random.Next(3);
+                switch ((int)empCheck)
+                {
+                    case IS_FULL_TIME:
+                        Console.WriteLine("Employee is Doing Full Time:");
+                        empHrs = 8;
+                        break;
+                    case IS_PART_TIME:
+                        Console.WriteLine("Employee is Doing Part Time:");
+                        empHrs = 4;
+                        break;
+                    default:
+                        Console.WriteLine("Employee is Absent:");
+                        empHrs = 0;
+                        break;
+                }
+                empWagePerDay = empHrs * EMP_WAGE_PER_HOUR;
+                empWagePerMonth += empWagePerDay;
+                Console.WriteLine("Employee Hours: " + empHrs);
+                Console.WriteLine("Employee Wage per Day: " + empWagePerDay);
             }
-            empWage = empHrs * EMP_WAGE_PER_HOUR;
-            Console.WriteLine("Employee Hours: " + empHrs);
-            Console.WriteLine("Employee Wage: " + empWage);
+            Console.WriteLine("Emp Wage Per Month : " + empWagePerMonth);
         }
     }
 }
