eb0e62105b66ef54041de1b894a5f621aef39524
sanjana8100 <sanjanams812@gmail.com>
2023-06-10 13:23:09 +0530

    Employee Wage Program

---
+++ Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace DND3_EmployeeWageProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
