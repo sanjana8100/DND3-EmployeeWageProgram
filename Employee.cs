953526736b897cd68fa89fa81f9e6c3faede69b0
sanjana8100 <sanjanams812@gmail.com>
2023-06-10 16:12:40 +0530

    added: UC9

--- Employee.cs
+++ Employee.cs
@@ -4,11 +4,21 @@
     {
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
+        private string Company;
+        private int EmpWagePerHour, WorkingDaysPerMonth, TotalWorkingHours, totalWage;
 
-        public void ComputeWage(string Company, int EmpWagePerHour, int WorkingDaysPerMonth, int TotalWorkingHours)
+        public Employee(string Company, int EmpWagePerHour, int WorkingDaysPerMonth, int TotalWorkingHours)
         {
-            int empHrs, empWagePerDay, empWagePerMonth = 0, totalWorkingDays = 0, totalEmpHours = 0;
-            while (totalEmpHours <= TotalWorkingHours && totalWorkingDays < WorkingDaysPerMonth)
+            this.Company = Company;
+            this.EmpWagePerHour = EmpWagePerHour;
+            this.WorkingDaysPerMonth = WorkingDaysPerMonth;
+            this.TotalWorkingHours = TotalWorkingHours;
+        }
+
+        public void ComputeWage()
+        {
+            int empHrs = 0, empWagePerDay, empWagePerMonth = 0, totalWorkingDays = 0, totalEmpHours = 0;
+            while (totalEmpHours <= this.TotalWorkingHours && totalWorkingDays < this.WorkingDaysPerMonth)
             {
                 totalWorkingDays++;
                 Random random = new Random();
@@ -16,15 +26,12 @@
                 switch ((int)empCheck)
                 {
                     case IS_FULL_TIME:
-                        Console.WriteLine("Employee is Doing Full Time:");
                         empHrs = 8;
                         break;
                     case IS_PART_TIME:
-                        Console.WriteLine("Employee is Doing Part Time:");
                         empHrs = 4;
                         break;
                     default:
-                        Console.WriteLine("Employee is Absent:");
                         empHrs = 0;
                         break;
                 }
@@ -35,30 +42,15 @@
             }
             Console.WriteLine("**********************************************");
             Console.WriteLine("Emp Wage Per Month : " + empWagePerMonth);
+            totalWage = empWagePerMonth * 12;
+            Console.Write("Total Wage of the Employee Per Year: " + totalWage);
         }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program On Master Branch");
             Console.WriteLine("---------------------------------------------------------------");
-            Console.WriteLine("Enter the Company Name: \n1.TCS\n2.Cognizant\n3.Infosys");
-            int company = Convert.ToInt32(Console.ReadLine());
-            Employee employee = new Employee();
-            switch (company)
-            {
-                case 1:
-                    employee.ComputeWage("TCS",300, 20, 100);
-                    break;
-                case 2:
-                    employee.ComputeWage("Cognizant",500, 23, 120);
-                    break;
-                case 3:
-                    employee.ComputeWage("Infosys",200, 25, 150);
-                    break;
-                default:
-                    Console.WriteLine("Please choose a valid company.");
-                    break;
-            }
-
+            Employee employee = new Employee("TCS",300,20,100);
+            employee.ComputeWage();
         }
     }
 }
