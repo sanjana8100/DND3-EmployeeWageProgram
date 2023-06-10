8b1f2ec0e52f651c89e394f76e6c3b375deb0e08
sanjana8100 <sanjanams812@gmail.com>
2023-06-10 14:13:36 +0530

    added: UC4

--- Employee.cs
+++ Employee.cs
@@ -2,32 +2,32 @@
 {
     internal class Employee
     {
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int EMP_WAGE_PER_HOUR = 20;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program On Master Branch");
-            Console.WriteLine("Checking whether Employee is present or absent...");
-            int IsFullTime = 1;
-            int IsPartTime = 2;
-            int EmpWagePerHour = 20;
+
             int empHrs, empWage;
             Random random = new Random();
             double empCheck = random.Next(3);
-            if (empCheck == IsFullTime)
+            switch ((int)empCheck)
             {
-                Console.WriteLine("Employee is working Full Time.");
-                empHrs = 8;
+                case IS_FULL_TIME:
+                    Console.WriteLine("Employee is Doing Full Time:");
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    Console.WriteLine("Employee is Doing Part Time:");
+                    empHrs = 4;
+                    break;
+                default:
+                    Console.WriteLine("Employee is Absent:");
+                    empHrs = 0;
+                    break;
             }
-            else if (empCheck == IsPartTime)
-            {
-                Console.WriteLine("Employee is working Part Time.");
-                empHrs = 4;
-            }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
-                empHrs = 0;
-            }
-            empWage = empHrs * EmpWagePerHour;
+            empWage = empHrs * EMP_WAGE_PER_HOUR;
             Console.WriteLine("Employee Hours: " + empHrs);
             Console.WriteLine("Employee Wage: " + empWage);
         }
