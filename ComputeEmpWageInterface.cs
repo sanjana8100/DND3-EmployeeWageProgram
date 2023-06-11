75033f75e24947ebb454c5c1273eba66356c85ab
sanjana8100 <sanjanams812@gmail.com>
2023-06-11 19:48:48 +0530

    added: UC11

---
+++ ComputeEmpWageInterface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DND3_EmployeeWageProgram
+{
+    internal interface ComputeEmpWageInterface
+    {
+        public void AddCompanyEmpWage(string Company, int EmpWagePerHour, int WorkingDaysPerMonth, int TotalWorkingHours);
+        public void ComputeEmpWage();
+    }
+}
